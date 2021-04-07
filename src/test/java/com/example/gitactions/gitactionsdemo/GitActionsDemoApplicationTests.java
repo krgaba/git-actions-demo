bb2ea46a7aeca11993247668f614b3e4d15cbcb4
Khuram Gaba <krgaba@gmail.com>
2021-04-07 12:34:57 -0500

    init

---
+++ src/test/java/com/example/gitactions/gitactionsdemo/GitActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactions.gitactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
